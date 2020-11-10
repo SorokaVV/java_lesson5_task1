1116dbc9ab504d8cbf92045ef17273d518ebb100
Vladimir Soroka <40acopoka@gmail.com>
2020-11-10 20:01:34 +0300

    add bonus-calculator

---
+++ bonus-calculator/src/test/java/BonusServiceTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    void shouldCalculateRegistredAndUnderLimit() {
+        BonusService service = new BonusService();
+        long amount = 1000_60;
+        boolean registered = true;
+        long expected = 30;
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateRegistredAndOverLimit() {
+        BonusService service = new BonusService();
+        long amount = 1_000_000_60;
+        boolean registered = true;
+        long expected = 500;
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateNotRegistredAndUnderLimit() {
+        BonusService service = new BonusService();
+        long amount = 1000_60;
+        boolean registered = false;
+        long expected = 10;
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateNotRegistredAndOverLimit() {
+        BonusService service = new BonusService();
+        long amount = 1_000_000_60;
+        boolean registered = false;
+        long expected = 500;
+        long actual = service.calculate(amount, registered);
+        assertEquals(expected, actual);
+    }
+}
